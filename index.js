3276559ffef302d14d3e115b07e5171b6da936ba
jasonronalddavis <jdavis@c.ringling.edu>
2021-08-25 00:30:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Your code here
+function mapToNegativize(Myarray){
+    let altArray = []
+    Myarray.forEach(num => altArray.push(num * -1));
+return altArray
+};
+
+function mapToNoChange(Myarray) {
+    return Myarray;
+  };
+
+
+  function mapToDouble(Myarray) {
+    let altArray = [];
+    for (const num of Myarray) {
+      altArray.push(num * 2)
+    };
+    return altArray;
+  };
+
+  function mapToSquare(Myarray) {
+    let altArray = [];
+    for (const num of Myarray) {
+      altArray.push(num ** 2)
+    };
+    return altArray;
+  };
+
+
+  function reduceToTotal(Myarray, startPoint = 0) {
+    let total = startPoint;
+    for(const elem of Myarray) {
+        total += elem;
+    };
+
+    return total;
+}
+
+function reduceToAllTrue(Myarray) {
+    let value = true;
+    for(const elem of Myarray) {
+        if (elem == false) {
+            value = false;
+        };
+    };
+    return value;
+}
+
+
+function reduceToAnyTrue(Myarray) {
+    let value = false;
+    for(const elem of Myarray) {
+        if (elem == true) {
+            value = true;
+        };
+    };
+    return value;
+}
